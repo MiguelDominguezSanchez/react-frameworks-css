cb27044e59bc9eca62d2724eb1d2aa86d478a088
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-15 12:37:50 +0200

    26. React Bootstrap & Material UI

---
+++ src/components/MaterialUI.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import MaterialCard from './MaterialCard'
+import MaterialDrawer from './MaterialDrawer'
+
+export default function MaterialUI() {
+	return (
+		<Fragment>
+			<h2>Material UI</h2>
+			<MaterialCard />
+			<MaterialDrawer />
+		</Fragment>
+	)
+}
